48fdacd3fa44c4302b65785d5fa90767e7fed52b
Moric <eugeniu.morarescu@wu.com>
2018-11-20 16:43:33 +0200

    Add dependency : cucumber-spring to fix the issue with null object.

--- src/main/java/com/atf/AppConfig.java
+++ src/main/java/com/atf/AppConfig.java
@@ -2,6 +2,7 @@ package com.atf;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @ComponentScan("com.atf")
 public class AppConfig {
